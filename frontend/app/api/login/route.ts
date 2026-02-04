627c56b16909ec41e01819c71b6cb2f7f74daca0
LaureLavie <laure.lavie@gmail.com>
2026-02-04 13:52:11 +0100

    register route et login route

---
+++ frontend/app/api/login/route.ts
@@ -0,0 +1,15 @@
+export const login = async (email: string, password: string) => {
+  const response = await fetch("http://localhost:8000/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to login");
+  }
+
+  return response.json();
+};
