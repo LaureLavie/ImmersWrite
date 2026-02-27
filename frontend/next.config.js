2a698bfcba6dbfdf1105cd728d48ba746818259b
LaureLavie <laure.lavie@gmail.com>
2026-02-27 15:23:02 +0100

    navbar gap + cloudinary image url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
