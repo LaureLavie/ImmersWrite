466cfd644301008e16f0a818224cacce96c4049c
LaureLavie <laure.lavie@gmail.com>
2026-02-27 15:23:39 +0100

    schema.py+ modele+ cors

--- backend/database.py
+++ backend/database.py
@@ -12,8 +12,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL)
-
+engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
